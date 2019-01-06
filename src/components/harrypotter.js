77d289d8042248b54b0e03930db535c532bf2d0f
Peter Jin <jzy521@Zheyangs-MacBook-Air.local>
2019-01-05 17:31:39 -0800

    example

--- src/components/harrypotter.js
+++ src/components/harrypotter.js
@@ -1,23 +1,59 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import {HeaderNav} from './headerNav';
+import { HeaderNav } from './headerNav';
 
-export class Harrypotter extends React.Component{
+export class Harrypotter extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state={
+      computer: 'mac'
+    }
+    console.log('constructor');
   }
-  render(){
+    
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+
+   getSnapshotBeforeUpdate(){
+    console.log('getSnapshotBeforeUpdate');
+   }
+
+   componentDidUpdate(){
+    console.log('componentDidUpdate');
+    alert('item change');
+
+   }
+
+   componentWilllMount(){
+    console.log('componentWilllMount');
+   }
+
+   componentDidMount(){
+    console.log('componentDidMount');
+   
+   }
+
+  render() {
+    
+    console.log('render');
+    // if(this.state.abc === '123'){
+    //   this.setState({abc: '345'}, () => {
+    //     console.log('after state change : '+ this.state.abc);
+    //   });
+    // }
+    
     let input;
-    console.log('rrrr ggg : '+JSON.stringify(this.props));
+    //console.log('rrrr ggg : ' + JSON.stringify(this.props));
     return (
       <div>
-        <HeaderNav/>
-  
+        <HeaderNav />
+
         <div className="App flex-container">
-           <div>id : {this.props.id} ,</div>
-           <br/>
-           <div> text : {this.props.text}</div>
+          <div>id : {this.props.id} ,</div>
+          <br />
+          <div> text : {this.props.text}</div>
         </div>
         <form onSubmit={e => {
           e.preventDefault()
@@ -32,9 +68,10 @@ export class Harrypotter extends React.Component{
             Add Todo
           </button>
         </form>
-         
+
       </div>
     )
-}}
+  }
+}
 
 export default Harrypotter;
